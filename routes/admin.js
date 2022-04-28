37c81c6de6ecccecb5b9e5fb53be898169e506e4
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-04-28 19:27:47 +0300

    .

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,7 @@ router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
+
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
