70157863b337141e2fb16ebe14fe05abe4ed0f95
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-04-29 11:29:37 +0300

    added some styling

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRouter = require('./routes/admin.js');
+const adminData = require('./routes/admin.js');
 const shopRouter = require('./routes/shop.js');
 const path = require('path');
 
@@ -9,11 +9,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRouter);
+app.use('/admin', adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const rootDir = require('../util/path.js');
+const adminData = require('./admin.js');
 
 router.get('/', (req, res, next) => {
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
