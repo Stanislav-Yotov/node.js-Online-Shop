ca351498f3b6377f5405d4ef56532fe97e294f9b
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-05-03 13:46:05 +0300

    added Details view

--- routes/shop.js
+++ routes/shop.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const shopController = require('../controllers/shop.js');
 
 router.get('/', shopController.getIndex);
-
 router.get('/products', shopController.getProducts);
+router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
-router.get('/checkout', shopController.getCheckout);
 router.get('/orders', shopController.getOrders);
+router.get('/checkout', shopController.getCheckout);
+
 
 
 module.exports = router;
