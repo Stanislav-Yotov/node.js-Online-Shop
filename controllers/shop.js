43155c9dac96d8d83a963d7856d98185b7f92aec
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-05-03 11:02:08 +0300

    added some views

--- controllers/shop.js
+++ controllers/shop.js
@@ -34,3 +34,11 @@ exports.getCheckout = (req, res, next) => {
   })
 };
 
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders',  {
+    path: '/orders',
+    pageTitle: 'Orders',
+  })
+};
+
+
