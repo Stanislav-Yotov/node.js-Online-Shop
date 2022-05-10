cc74fd209df181fb4d2e64cca3be940ab6da269e
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-05-10 15:38:54 +0300

    .

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'stakata1490', { dialect: 'mysql', host: 'localhost' });
+const sequelize = new Sequelize('node-complete', 'root',  { dialect: 'mysql', host: 'localhost' });
 
 module.exports = sequelize;
