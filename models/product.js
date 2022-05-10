d36b9eee346d2cc1372e661ffc73192b62d0d035
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-05-10 15:38:20 +0300

    rewriting the product model, using Sequelize

--- models/product.js
+++ models/product.js
@@ -1,31 +1,33 @@
-const Cart = require('./cart.js');
-const db = require('../util/database.js');
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database.js');
 
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
 
-module.exports = class Product {
-    constructor(id, title, imageUrl, price, description) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.description = description;
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
+});
 
-    save() {
-        return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', 
-        [this.title, this.price, this.imageUrl, this.description]
-        );
-    };
-
-    static fetchAll() {
-        return db.execute('SELECT * FROM products');
-    };
-
-    static findById(id){
-       return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-    }
-
-    static deleteById(id){
-        
-    }
-}
+module.exports = Product;
