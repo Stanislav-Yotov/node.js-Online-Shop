bd786fc792c80211395d4d5040660dbc253e22b9
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-05-16 14:32:59 +0300

    added the cart items

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('62820417fbc9a7ac191ae6a5')
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => console.log(err));
