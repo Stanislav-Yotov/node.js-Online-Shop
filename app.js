e04d56c599d8f6bb0b5e04e28c9e9e8dd4bea8e1
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-05-17 13:48:19 +0300

    adding mongoose

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+//const User = require('./models/user');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('6282456afbc9a7ac191ae6b2')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('6282456afbc9a7ac191ae6b2')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
