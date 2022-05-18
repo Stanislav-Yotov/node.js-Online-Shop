5d36773c54ea95ab34adb681dbd03a729702a20f
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-05-18 12:13:32 +0300

    added remove from cart

--- models/user.js
+++ models/user.js
@@ -17,15 +17,15 @@ const userSchema = new Schema({
         productId: {
           type: Schema.Types.ObjectId,
           ref: 'Product',
-          
+
         },
-        quantity: { type: Number}
+        quantity: { type: Number }
       }
     ]
   }
 });
 
-userSchema.methods.addToCart = function(product) {
+userSchema.methods.addToCart = function (product) {
   const cartProductIndex = this.cart.items.findIndex(cp => {
     return cp.productId.toString() === product._id.toString();
   });
@@ -47,6 +47,14 @@ userSchema.methods.addToCart = function(product) {
   this.cart = updatedCart;
   return this.save();
 };
+
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+}
 
 module.exports = mongoose.model('User', userSchema);
 
