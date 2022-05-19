26f4a7449a1412df14f743ea3744668208954755
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-05-19 19:02:36 +0300

    .

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const User = require('./models/user');
 const mongoose = require('mongoose');
 const MongoDBStore = require('connect-mongodb-session')(session);
 
-const MONGODB_URI = 'mongodb+srv://Stancho:nodecomplete@node-complete.u7g5c.mongodb.net/Node-Complete';
+
 
 const app = express();
 const store = new MongoDBStore({
