405ee6bb7082d54c49b0e395d1b23d88b2ccd452
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-05-29 10:52:41 +0300

    added validation

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const { check } = require('express-validator');
 
 const express = require('express');
 
@@ -7,18 +7,40 @@ const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-// /admin/add-product => GET
+
 router.get('/add-product', isAuth, adminController.getAddProduct);
 
-// /admin/products => GET
+
 router.get('/products', isAuth, adminController.getProducts);
 
-// /admin/add-product => POST
-router.post('/add-product', isAuth, adminController.postAddProduct);
+
+router.post('/add-product', [
+    check('title')
+        .isString()
+        .isLength({ min: 3 })
+        .trim(),
+    check('imageUrl').isURL(),
+    check('price').isFloat(),
+    check('description')
+    .isLength({ min: 5, max: 400 })
+    .trim()
+
+], isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product', [
+    check('title')
+        .isString()
+        .isLength({ min: 3 })
+        .trim(),
+    check('imageUrl').isURL(),
+    check('price').isFloat(),
+    check('description')
+    .isLength({ min: 5, max: 400 })
+    .trim()
+
+], isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
