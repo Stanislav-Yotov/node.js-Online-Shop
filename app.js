d735dea9d718919fdb0005feb4de4ff758d5a41d
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-05-31 16:16:03 +0300

    .

--- app.js
+++ app.js
@@ -24,9 +24,16 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
+    cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
   }
 });
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  }
+  cb(null, false);
+};
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -36,8 +43,10 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({storage: fileStorage}).single('image'));
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'images')));
+
 app.use(
   session({
     secret: 'my secret',
